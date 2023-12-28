fb1359f5a7daf39afbe9d5383129befa24975f5f
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2023-12-28 08:55:18 +0530

    final editt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,13 +14,13 @@ export default function Navbar(props) {
   }, [location])
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent" style={{ boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)' }} >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-transparent" style={{ boxShadow: '0 4px 6px rgba(0, 0, 0, 0.5)' }} >
         <div className="container-fluid " >
           <Link className="navbar-brand" style={{color:"black"}} to="/">NoteBook</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse"  id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} style={{color:"black"}} aria-current="page" to="/">Home</Link>
