37d4382c374e7861ac66fd43864012805a29eb44
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2024-06-17 04:04:37 +0530

    url

--- src/App.js
+++ src/App.js
@@ -4,53 +4,39 @@ import { Home } from './components/Home';
 import About from './components/About';
 import Alert from './components/Alert';
 import NoteState from './context/notes/NoteState';
-import {
-  Routes,
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
 import Signup from './components/Signup';
 import Login from './components/Login';
 import { useState } from 'react';
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
-  const [alert,setAlert]=useState(null);
-  const showAlert = (message,type)=>{
+  const [alert, setAlert] = useState(null);
+  const backendUrl = "https://notebook-api-flame.vercel.app/";
+
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    })
-    setTimeout(()=>{
+    });
+    setTimeout(() => {
       setAlert(null);
-    },2000)
-  }
+    }, 2000);
+  };
+
   return (
     <NoteState>
-       
-    <Router>
+      <Router>
         <Navbar showAlert={showAlert} />
-        <Alert alert= {alert }/>
+        <Alert alert={alert} />
         <div className="container">
-
-      <Routes>
-        <Route path="/" element={<Home showAlert={showAlert}/>} />
-      </Routes>
-
-      <Routes>
-        <Route path="/about" element={<About />} />
-
-      </Routes>
-      <Routes>
-        <Route path="/signup" element={<Signup showAlert={showAlert}/>} />
-
-      </Routes>
-      <Routes>
-        <Route path="/login" element={<Login showAlert={showAlert}/>} />
-
-      </Routes>
-
+          <Routes>
+            <Route path="/" element={<Home showAlert={showAlert} backendUrl={backendUrl} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signup" element={<Signup showAlert={showAlert} backendUrl={backendUrl} />} />
+            <Route path="/login" element={<Login showAlert={showAlert} backendUrl={backendUrl} />} />
+          </Routes>
         </div>
-    </Router >
-    
+      </Router>
     </NoteState>
   );
 }
